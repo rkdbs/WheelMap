bafb4f551aeb9818152741b07517bd68a834dc43
seonyo <jsy6535@naver.com>
2023-09-05 01:51:43 +0900

    :sparkles:[Feat] index파일 위치를 현재 위치로 뜨기 수정

--- WheelMap-deploy/public/JS/index.js
+++ WheelMap-deploy/public/JS/index.js
@@ -1,59 +1,55 @@
-    // 페이지가 로딩이 된 후 호출하는 함수입니다.
-    function initTmap(){
-    // map 생성
-    // Tmapv3.Map을 이용하여, 지도가 들어갈 div, 넓이, 높이를 설정합니다.
-    var map = new Tmapv3.Map("map_div", { // 지도가 생성될 div
-        center : new Tmapv3.LatLng(37.46671695320371, 126.93308346538122),
-        width : "100%",	// 지도의 넓이
-        height : "100%",	// 지도의 높이
-        zoom : 16	// 지도 줌레벨
+var map;
+ var latitude;
+ var longitude;
+
+ //위치 정보를 가져오는 비동기 함수
+ function getUserLocation() {
+    return new Promise(function(resolve, reject){
+        navigator.geolocation.getCurrentPosition(function(position){
+            latitude = position.coords.latitude;
+            longitude = position.coords.longitude;
+            resolve();
+          }, function(error){
+            reject(error);
+        });
     });
-    //2. 시작 심볼 찍기
-    marker_s = new Tmapv3.Marker(
-        {
-        position: new Tmapv3.LatLng(37.46671695320371, 126.93308346538122),
-        icon: "image/MyLocationMarker.png",
-        iconSize: new Tmapv3.Size(14, 14),
-        map: map
-        }
-    )
+ }
+
+   // 페이지가 로딩이 된 후 호출하는 함수입니다.
+   function initializeMap(){
+    navigator.geolocation.getCurrentPosition(detectLocationChange);
+    // api 불러오기
+    var headers = {};
+    headers["appKey"] = "q1hz24YqUC7g84TRhAW3v8a52xq51B3472o9tPeF";
+
+   // map 생성
+   // Tmapv3.Map을 이용하여, 지도가 들어갈 div, 넓이, 높이를 설정합니다.
+    map = new Tmapv2.Map("map", { // 지도가 생성될 div
+       center : new Tmapv2.LatLng(latitude, longitude),
+       width : "100%",   // 지도의 넓이
+       height : "95%",   // 지도의 높이
+       zoom : 16   // 지도 줌레벨
+   });
+   // 시작 심볼 찍기
+   marker_s = new Tmapv2.Marker(
+   {
+   position: new Tmapv2.LatLng(latitude, longitude),
+   icon: "image/MyLocationMarker.png",
+   iconSize: new Tmapv2.Size(14, 14),
+   map: map
+   }
+   )
 }
-    function getKakaoToken(code) {
-        let token = "";
-        let data = {
-            "grant_type": "authorization_code",
-            "client_id": REST_API_KEY,
-            "redirect_uri": redirectUrl,
-            "code": code,
-        };
 
-        $.ajax(kakaoTokenApiUrl, {
-            data: data,
-            dataType: "json",
-            method: "POST",
-            async: false,
-            success: function (resultData) {
-            token = resultData.access_token;
-            console.log(token);
-            }
-        }); 
-
-        return token;
+async function initTmap(){
+    try{
+        await getUserLocation();
+        initializeMap();
+    }catch (error){
+        console.log("Error");
     }
-
-
-    // 3-5. 회원 정보 가져오기
-function getKakaoUserInfo(token) {
-	$.ajax(kakaoUserApiUrl, {
-            headers: {
-            'Authorization': `Bearer ${token}`,
-            },
-            contentType: "application/x-www-form-urlencoded;charset=utf-8",
-	        dataType: "json",
-            data: JSON.stringify({}),
-	        method: "POST",
-	        async: false,
-	        success: function (resultData) {
-	           console.log(resultData);
-	    }});
+}
+function detectLocationChange({ coords, timestamp }) {
+    latitude = coords.latitude;   // 위도
+    longitude = coords.longitude; // 경도
 }
